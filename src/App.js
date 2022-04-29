b60d620f06b71f5c47a6eff238dd3940f3b91016
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-29 19:08:27 +0600

    complete setup components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Gallery from "./Components/Gallery/Gallery";
 import Header from "./Components/Home/Header/Header/Header";
 import Home from "./Components/Home/Home/Home";
+import Reviews from "./Components/Reviews/Reviews";
+import Services from "./Components/Services/Services";
+import Blog from "./Components/Blog/Blog";
+import ManageCars from "./Components/ManageCars/ManageCars";
+import MyCars from "./Components/MyCars/MyCars";
+import AddCar from "./Components/AddCar/AddCar";
+import Login from "./Components/Login/Login";
+import Logout from "./Components/Logout/Logout";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/Reviews" element={<Reviews />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/ManageCars" element={<ManageCars />} />
+        <Route path="/MyCars" element={<MyCars />} />
+        <Route path="/AddCar" element={<AddCar />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+      </Routes>
     </div>
   );
 }
--- src/Components/Home/Header/Header/Header.js
+++ src/Components/Home/Header/Header/Header.js
@@ -1,21 +1,32 @@
 import React from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import logo from "../../../../img/logo.png";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
       <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Container className="mt-3 mb-3">
+          <Navbar.Brand as={Link} to="/" className="logo">
+            {" "}
+            CAR VALLEY{" "}
+          </Navbar.Brand>
           <div>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link as={Link} to="/" href="#home">
-                  Home
-                </Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
+              <Nav className="me-auto nav">
+                <Link to="/">home</Link>
+                <Link to="Gallery">Gallery</Link>
+                <Link to="Reviews">Reviews</Link>
+                <Link to="Services">Services</Link>
+                <Link to="Blog"> Blog</Link>
+                <Link to="ManageCars">Manage Cars</Link>
+                <Link to="MyCars">My Cars</Link>
+                <Link to="AddCar">Add Car</Link>
+                <Link to="Login">Login</Link>
+                <Link to="Logout">Logout</Link>
               </Nav>
             </Navbar.Collapse>
           </div>
