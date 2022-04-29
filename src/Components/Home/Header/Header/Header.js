d248b77f1cd0836ecc41dddcb02516c7a3fd1e59
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-29 16:14:04 +0600

    added slider

--- src/Components/Home/Header/Header/Header.js
+++ src/Components/Home/Header/Header/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/" href="#home">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Container>
+      </Navbar>
     </div>
   );
 };
---
+++ src/Components/Home/Slider/Carousel.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Carousel } from "react-bootstrap";
+import bg1 from "../../../img/bg/bg3.jpg";
+import bg2 from "../../../img/bg/bg2.jpg";
+import bg3 from "../../../img/bg/bg5.jpg";
+
+const Slider = () => {
+  return (
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bg1} alt="First slide" />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bg2} alt="Second slide" />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={bg3} alt="Third slide" />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Slider;
