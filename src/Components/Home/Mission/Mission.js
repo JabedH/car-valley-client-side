462f1ce7ca106469e02586468400e549d557f32e
JabedH <87475551+JabedH@users.noreply.github.com>
2022-04-30 17:00:30 +0600

    completed mission section

--- src/Components/Home/Mission/Mission.js
+++ src/Components/Home/Mission/Mission.js
@@ -8,11 +8,13 @@ import img3 from "../../../img/mission/3.jpg";
 const Mission = () => {
   return (
     <div className="mission container">
-      <div>
-        <h1>Our Mission</h1>
+      <div className="full-mission">
+        <div className="mission-text">
+          <h1>Our Mission</h1>
+        </div>
         <h3>
-          At Car Valley, we want to provide you with the best automotive
-          experience possible.
+          At Car Valley, we want to provide you with the best Car experience
+          possible.
         </h3>
         <p>
           From researching the vehicle you want to test driving your options,
@@ -24,28 +26,12 @@ const Mission = () => {
         <Carousel>
           <Carousel.Item>
             <img className="d-block w-100" src={img1} alt="First slide" />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src={img2} alt="Second slide" />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img className="d-block w-100" src={img3} alt="Third slide" />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </div>
