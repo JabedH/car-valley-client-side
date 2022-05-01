b30b1ab8aee9e360eb41df9f924c7c0892088ee8
JabedH <87475551+JabedH@users.noreply.github.com>
2022-05-02 00:28:31 +0600

    complete social login

--- src/Components/SocialLogin/SoccialLogina.js
+++ src/Components/SocialLogin/SoccialLogina.js
@@ -3,20 +3,27 @@ import google from "../../img/icon/google.svg";
 import fb from "../../img/icon/fb.svg";
 import github from "../../img/icon/githiub.svg";
 import auth from "../../firebase.init";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSignInWithFacebook,
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 
 const SocialLogin = () => {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGithub, user1, loading1, error1] = useSignInWithGithub(auth);
+  const [signInWithFacebook, user2, loading2, error2] =
+    useSignInWithFacebook(auth);
   return (
     <div onClick={() => signInWithGoogle()} className="social-icon">
       <button>
         <img src={google} alt="" />
       </button>
       <button>
-        <img src={fb} alt="" />
+        <img onClick={() => signInWithFacebook()} src={fb} alt="" />
       </button>
       <button>
-        <img src={github} alt="" />
+        <img onClick={() => signInWithGithub()} src={github} alt="" />
       </button>
     </div>
   );
