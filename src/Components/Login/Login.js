68e7f33bc0d5729f423843296e976cf5355534c5
JabedH <87475551+JabedH@users.noreply.github.com>
2022-05-02 03:18:16 +0600

    added signin and signup

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 import SocialLogin from "../SocialLogin/SoccialLogina";
 import "./Login.css";
 
 const Login = () => {
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const Handlelogin = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    signInWithEmailAndPassword(email, password);
+  };
   return (
     <div>
       <div>
-        <Form className="w-25 mx-auto text-start">
+        <Form onSubmit={Handlelogin} className="w-25 mx-auto text-start">
           <input
             name="email"
             className="w-100 mt-3"
@@ -25,7 +35,7 @@ const Login = () => {
           />
           <div className="login-btn">
             <button className="w-100 btn-color mt-3" type="submit">
-              Submit
+              Login
             </button>
           </div>
           <div className="mt-3 text-center">
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,9 +1,62 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React from "react";
+import { Form } from "react-bootstrap";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link } from "react-router-dom";
+import auth from "../../firebase.init";
 
 const Signup = () => {
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+  const handleSignup = (event) => {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    // const confirmPass = event.target.confirmPass.value;
+    createUserWithEmailAndPassword(email, password);
+  };
   return (
     <div>
-      <h1>Signup</h1>
+      <Form onSubmit={handleSignup} className="w-25 mx-auto text-start">
+        <input
+          name="name"
+          className="w-100 mt-3"
+          type="text"
+          placeholder="Name"
+          required
+        />
+        <input
+          name="email"
+          className="w-100 mt-3"
+          type="email"
+          placeholder="Enter email"
+          required
+        />
+        <input
+          name="password"
+          className="w-100 mt-3"
+          type="password"
+          placeholder="Password"
+          required
+        />
+        {/* <input
+          name="password"
+          className="w-100 mt-3"
+          type="password"
+          placeholder="confirm Password"
+          required
+        /> */}
+        <div className="login-btn">
+          <button className="w-100 btn-color mt-3" type="submit">
+            Sing Up
+          </button>
+        </div>
+        <div className="mt-3 text-center">
+          <Link to="/login" className="text-decoration-none  ">
+            Already you have an account?
+          </Link>
+        </div>
+      </Form>
     </div>
   );
 };
