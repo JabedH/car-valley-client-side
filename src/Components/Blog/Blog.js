bfc937f8dbecf5f0f3b1d017ca8d5b7ddcbaa7d3
JabedH <87475551+JabedH@users.noreply.github.com>
2022-05-08 09:34:43 +0600

    added blog section artical

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -5,14 +5,37 @@ const Blog = () => {
     <div>
       <div>
         <h1>Difference between Node.JS and Javascript</h1>
-        NodeJS is a Javascript runtime environment that allows the javascript to
-        be run on the server-side. Javascript is a Scripting language it also a
-        high-level programming language. Javascript can only be run in the
-        browsers.Nodejs allows Javascript code to run outside the browser. It is
-        basically used on the client-side.It is mostly used on the server-side.
-        JS have capability to add HTML tags. Nodejs does not have capability to
-        add HTML tags. Javascript can run in any browser engine. Javascript is
-        used in frontend development and node.js is used in backend development.
+        <p>
+          NodeJS is a Javascript runtime environment that allows the javascript
+          to be run on the server-side. Javascript is a Scripting language it
+          also a high-level programming language. Javascript can only be run in
+          the browsers.Nodejs allows Javascript code to run outside the browser.
+          It is basically used on the client-side.It is mostly used on the
+          server-side. JS have capability to add HTML tags. Nodejs does not have
+          capability to add HTML tags. Javascript can run in any browser engine.
+          Javascript is used in frontend development and node.js is used in
+          backend development.
+        </p>
+      </div>
+      <div>
+        <h1>When should we use node.js and mongodb?</h1>
+        <h5>
+          MongoDB and NodeJS are two different technologies.These two
+          technologies are for different parts of web server system. We don't
+          substitute one for the other.
+        </h5>
+        <p>
+          When should we use Nodejs? When should we use Nodejs? NodeJS is a
+          Javascript runtime environment. It's actually helps JavaScript to run
+          outside of server.NodeJS helps us to to connect our client site to
+          database by it's server site.
+        </p>
+        <p>
+          When should we use MongoDB? IF our application needs to storage data
+          that can effortlessly edit or update it late, then we need database.
+          there are so many popular databases. MongoDB one of them. MongoDB is a
+          No-SQL database for storing data.
+        </p>
       </div>
     </div>
   );
